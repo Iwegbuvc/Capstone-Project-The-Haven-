e300c86e6cb81f3d2b0c2584e02af30c44f04a1a
unknown <adeyemioluwakayode73@gmail.com>
2024-02-02 01:48:19 +0100

    haven..

--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -7,6 +7,8 @@ export default function SignUp() {
   const [lName, setLName] = useState('');
   const [bName, setBName] = useState('');
   const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
 
   return (
     <>
@@ -17,12 +19,15 @@ export default function SignUp() {
       <div className='rounded-2xl bg-white py-12 px-6 mt-10 w-[450px] max-w-[450px]'>
         <h1 className='text-[#0602B2] text-[32px] font-bold'>SIGN UP NOW</h1>
         <form  className='flex flex-col gap-2'>
-    <div className='flex'>
-    <label >First Name*</label>
-      <input type="text" className='text-[#0602B2] border-2 border-[#9D9D9D] rounded-lg w-[50%]' value={fName} onChange={(e) => setFName(e.target.value)} required />
+    <div className='flex '>  
+     <div className='flex flex-col'>
+       <label >First Name*</label>
+     <input type="text" className='text-[#0602B2] border-2 border-[#9D9D9D] rounded-lg w-[90%]' value={fName} onChange={(e) => setFName(e.target.value)} required />
+     </div>
 
-      <label>Last Name*</label>
-      <input type="text" className='text-[#0602B2] border-2 border-[#9D9D9D] rounded-lg w-[50%]'  value={lName}  onChange={(e) => setLName(e.target.value)} required />
+      <div className='flex flex-col'>
+        <label>Last Name*</label>
+      <input type="text" className='text-[#0602B2] border-2 border-[#9D9D9D] rounded-lg w-[90%]'  value={lName}  onChange={(e) => setLName(e.target.value)} required /></div>
     </div>
 
       <label>Business Name</label>
@@ -31,13 +36,23 @@ export default function SignUp() {
       <label>Email Address*</label>
       <input type="email" className='text-[#0602B2] border-2 border-[#9D9D9D] rounded-lg' value={email}   onChange={(e) => setEmail(e.target.value)} required />
 
+      <div className='flex '>  
+     <div className='flex flex-col'>
+       <label >Password*</label>
+     <input type="password" className='text-[#0602B2] border-2 border-[#9D9D9D] rounded-lg w-[90%]' value={password} onChange={(e) => setPassword(e.target.value)} required />
+     </div>
+
+      <div className='flex flex-col'>
+        <label>Confirm Password*</label>
+      <input type="password" className='text-[#0602B2] border-2 border-[#9D9D9D] rounded-lg w-[90%]'  value={confirmPassword}  onChange={(e) => setConfirmPassword(e.target.value)} required /></div>
+    </div>
 
       
 
     </form>
         
       </div>
-      <div>
+      <div className=''>
         <h1>UNLOCK A WORLD OF POSSIBILITIES BY SIGNING UP WITH SAFE HAVEN TODAY</h1>
 
        </div>
